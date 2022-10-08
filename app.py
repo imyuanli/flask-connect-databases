20aef93e64fd859054805c6c42b63ff656e88025
yuanli <2865437316@qq.com>
2022-10-08 18:05:44 +0800

    重构 测试及获取表接口 及 表的详细信息

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
+import json
+
+import demjson
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from impala.dbapi import connect
+from sqlalchemy import create_engine, inspect, MetaData, Table
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
-db = SQLAlchemy(app)  # 创建一个对象，设置名为db
-# 关闭数据库修改跟踪操作[提高性能]，可以设置为True，这样可以跟踪操作：
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# 开启输出底层执行的sql语句
-app.config['SQLALCHEMY_ECHO'] = True
-# 开启数据库的自动提交功能[一般不使用]
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 
 def connect_db(info, node_type):
     type_dict = {
         "Oracle": "oracle",
         "MySQL": "mysql+pymysql",
+        "Hive": "impala",
     }
     # node 信息
     driver = type_dict.get(node_type)
@@ -26,24 +25,16 @@ def connect_db(info, node_type):
     host = info.get('host')
     port = info.get('port')
     database = info.get('database')
+    # 连接方式
+    db_url = f"{driver}://{username}:{password}@{host}:{port}/{database}"
+    if node_type == "Hive":
+        db_url += '?auth_mechanism=PLAIN'
+    engine = create_engine(db_url)
+    return engine
 
-    # 不同数据库的连接方法
-    if node_type == 'Oracle' or node_type == 'MySQL':
-        app.config['SQLALCHEMY_DATABASE_URI'] = f"{driver}://{username}:{password}@{host}:{port}/{database}"
-        engine = db.get_engine()
-        conn = engine.connect()
-        conn.close()
-        return engine
-    elif node_type == "Hive":
-        conn = connect(host=host,
-                       port=port,
-                       auth_mechanism='PLAIN',
-                       user=username,
-                       password=password,
-                       database=database)
-        cur = conn.cursor()
-        cur.execute('SHOW TABLES')
-        return cur
+
+def ok(data):
+    return {"code": 200, "data": data}
 
 
 @app.route('/test_db_connect', methods=['POST'])
@@ -53,7 +44,7 @@ def test_db_connect():
     node_type = res['type']
     info = data_node[node_type]
     connect_db(info, node_type)
-    return jsonify({"message": "success"})
+    return jsonify(ok(""))
 
 
 @app.route('/get_tables', methods=['POST'])
@@ -61,23 +52,45 @@ def get_tables():
     res = request.json
     dataNode = res['dataNode']
     node_type = dataNode.get('type')
-    tables = ""
-    result = connect_db(dataNode, node_type)
-    obj = {}
-    if node_type == 'Oracle' or node_type == 'MySQL':
-        # 获取所有表
-        tables = result.table_names()
-    elif node_type == "Hive":
-        # 获取所有表
-        res = result.fetchall()
-        arr = []
-        for i in res:
-            arr.append(i[0])
-        tables = arr
-    return jsonify({
-        "code": 200,
-        "data": tables
-    })
+    engine = connect_db(dataNode, node_type)
+    insp = inspect(engine)
+    tables = insp.get_table_names()
+    return jsonify(ok(tables))
+
+
+@app.route('/get_columns_info', methods=['POST'])
+def get_columns_info():
+    res = request.json
+    data = res['data']
+    table = data.get('table')
+    data_node = data.get('dataNode')
+    node_type = data_node.get('type')
+    info = data_node[node_type]
+    # 创建连接
+    engine = connect_db(info, node_type)
+    insp = inspect(engine)
+    if insp:
+        columns = insp.get_columns(table)
+        meta = MetaData()
+        table = Table(table, meta, autoload=True, autoload_with=engine)
+        primaryKeyColNames = [pk_column.name for pk_column in table.primary_key.columns.values()]
+        # 处理数据
+        if len(primaryKeyColNames) == 1:
+            key = primaryKeyColNames[0]
+            for column in columns:
+                column['type'] = column['type'].__class__.__name__
+                if column['name'] == key:
+                    column['isPrimaryKey'] = 'true'
+                else:
+                    column['isPrimaryKey'] = 'false'
+        else:
+            for key in primaryKeyColNames:
+                for column in columns:
+                    if column['name'] == key:
+                        column['isPrimaryKey'] = 'true'
+                    else:
+                        column['isPrimaryKey'] = 'false'
+        return jsonify(ok(columns))
 
 
 if __name__ == '__main__':
